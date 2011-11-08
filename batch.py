69a081273224b043457305183acd248870c9a226
Wenxiang Chen--Laptop <chenwx.ustc@gmail.com>
2011-11-07 18:18:14 -0700

    Enable os.system in batch.py

--- batch.py
+++ batch.py
@@ -14,12 +14,12 @@ for n in [20, 50, 100] :
                             for q in [2, 4, 8, 16]:
                                 i = i+1
                                 print 'qsub sat.pbs -v P='+p+',A='+a+',F='+f+',I=1,S='+str(s)+',N='+str(n)+',K='+str(k)+',Q='+str(q)
-    #                            os.system( 'qsub sat.pbs -v P='+p+',A='+a+',N='+str(n)+',K='+str(k)+',Q='+str(q) )
+                                os.system( 'qsub sat.pbs -v P='+p+',A='+a+',F='+f+',I=1,S='+str(s)+',N='+str(n)+',K='+str(k)+',Q='+str(q) )
                         else:
                             q = 0
                             i = i+1
                             print 'qsub sat.pbs -v P='+p+',A='+a+',F='+f+',I=1,S='+str(s)+',N='+str(n)+',K='+str(k)+',Q='+str(q)
-#                        os.system( 'qsub sat.pbs -v P='+p+',A='+a+',N='+str(n)+',K='+str(k)+',Q='+str(q) )
+                            os.system( 'qsub sat.pbs -v P='+p+',A='+a+',F='+f+',I=1,S='+str(s)+',N='+str(n)+',K='+str(k)+',Q='+str(q) )
             else:
                 for s in [30, 100]:
                     for f in ['fit', 'mean', 'std']:
@@ -28,12 +28,12 @@ for n in [20, 50, 100] :
                                 for q in [2, 4, 8, 16]:
                                     i = i+1
                                     print 'qsub sat.pbs -v P='+p+',A='+a+',F='+f+',I=1,S='+str(s)+',N='+str(n)+',K='+str(k)+',Q='+str(q)
-        #                            os.system( 'qsub sat.pbs -v P='+p+',A='+a+',N='+str(n)+',K='+str(k)+',Q='+str(q) )
+                                    os.system( 'qsub sat.pbs -v P='+p+',A='+a+',F='+f+',I=1,S='+str(s)+',N='+str(n)+',K='+str(k)+',Q='+str(q) )
                             else:
                                 q = 0
                                 i = i+1
                                 print 'qsub sat.pbs -v P='+p+',A='+a+',F='+f+',I=1,S='+str(s)+',N='+str(n)+',K='+str(k)+',Q='+str(q)
-#                        os.system( 'qsub sat.pbs -v P='+p+',A='+a+',N='+str(n)+',K='+str(k)+',Q='+str(q) )
+                                os.system( 'qsub sat.pbs -v P='+p+',A='+a+',F='+f+',I=1,S='+str(s)+',N='+str(n)+',K='+str(k)+',Q='+str(q) )
 
 for a in ['LS', 'GA', 'CHC']:
     if a == 'LS':
@@ -41,9 +41,12 @@ for a in ['LS', 'GA', 'CHC']:
         for f in ['fit', 'mean', 'std']:
             i = i+1
             print 'qsub sat.pbs -v P='+p+',A='+a+',F='+f+',I=1,S='+str(s)+',N='+str(n)+',K='+str(k)+',Q='+str(q)
+            os.system( 'qsub sat.pbs -v P='+p+',A='+a+',F='+f+',I=1,S='+str(s)+',N='+str(n)+',K='+str(k)+',Q='+str(q) )
     else:
         for s in [30,100]:
             for f in ['fit', 'mean', 'std']:
                 i = i+1
                 print 'qsub sat.pbs -v P='+p+',A='+a+',F='+f+',I=1,S='+str(s)+',N='+str(n)+',K='+str(k)+',Q='+str(q)
+                os.system( 'qsub sat.pbs -v P='+p+',A='+a+',F='+f+',I=1,S='+str(s)+',N='+str(n)+',K='+str(k)+',Q='+str(q) )
+
 print i, 'jobs submitted'
