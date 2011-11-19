c33afe87ac93631427d5be56ecf45d94cbd3c494
Wenxiang Chen--Laptop <chenwx.ustc@gmail.com>
2011-11-18 22:05:43 -0700

    Reduced program instances.
    	Never need to run duplicate instances.

--- batch.py
+++ batch.py
@@ -1,6 +1,7 @@
 import os
 import pdb
 import random
+import math
 import time
 
 
@@ -14,24 +15,44 @@ class Struct:
     def __init__(self, **kwds):
         self.__dict__.update(kwds)
 
-def writeScript(p,a,f,i,s,n,k,q,count,scriptNo,chunkSize, track):
-    scriptNo = random.randint(0,len(track)-1)    
-    fName = 'run-'+str(track[scriptNo].num)+'.sh'
-    fileName = open(fName, 'a')
-    fileName.write('python demo.py '+p+' '+a+' '+f+' '+str(i)+' '+str(s)+' '+str(n)+' '+str(k)+' '+str(q)+' '+'&\n')
-    fileName.close()
+def resultExist(probName,algoName,fitName,inst,s,n,k,q):
+    """ check whether the results have been produced or not """
+    if probName == 'NKQ':
+        nameOfF = './result/'+probName+'-'+algoName+'-F'+fitName+'-I'+str(inst)+'-S'+str(s)+'-N'+str(n)+'-K'+str(k)+'-Q'+str(q)+'.txt'
+    elif probName == 'NK':
+        nameOfF = './result/'+probName+'-'+algoName+'-F'+fitName+'-I'+str(inst)+'-S'+str(s)+'-N'+str(n)+'-K'+str(k)+'.txt'
 
-    track[scriptNo].jobs = track[scriptNo].jobs + 1
-    if track[scriptNo].jobs == chunkSize:
-        # the script file is full
-        del track[scriptNo]
+    if os.path.isfile(nameOfF)==True:
+        print nameOfF, 'exists!!!'
+    return os.path.isfile(nameOfF)
+
+def countJobs(p,a,f,i,s,n,k,q,count):
+    if resultExist(p,a,f,i,s,n,k,q) == False:
+        return count + 1
+    else:
+        return count
+
+def writeScript(p,a,f,i,s,n,k,q,chunkSize, track):
+    if resultExist(p,a,f,i,s,n,k,q) == False:
+        scriptNo = random.randint(0,len(track)-1)    
+        fName = 'run-'+str(track[scriptNo].num)+'.sh'
+        fileName = open(fName, 'a')
+#        print 'python demo.py '+p+' '+a+' '+f+' '+str(i)+' '+str(s)+' '+str(n)+' '+str(k)+' '+str(q)+' '+'&\n'
+        fileName.write('python demo.py '+p+' '+a+' '+f+' '+str(i)+' '+str(s)+' '+str(n)+' '+str(k)+' '+str(q)+' '+'&\n')
+        fileName.close()
+
+        track[scriptNo].jobs = track[scriptNo].jobs + 1
+        if track[scriptNo].jobs == chunkSize:
+            # the script file is full
+            del track[scriptNo]
 
 def writeHeader(numOfjobs, chunkSize):
-    numOfScript = numOfjobs/chunkSize
+    numOfScript = int(math.ceil(numOfjobs/float(chunkSize)))
+    print 'header', numOfScript
     for i in range(numOfScript):
         fName = 'run-'+str(i+1)+'.sh'
         fileName = open(fName, 'w')
-        print >>fileName, '#!/bin/bash'
+        print >>fileName, '#!/bin/sh'
         fileName.close()
     # initialize tracking table
     track = []
@@ -40,14 +61,15 @@ def writeHeader(numOfjobs, chunkSize):
     return track
 
 def writeFooter(numOfjobs, chunkSize):
-    numOfScript = numOfjobs/chunkSize
+    numOfScript = int(math.ceil(numOfjobs/float(chunkSize)))
+    print 'footer', numOfScript
     for i in range(numOfScript):
         fName = 'run-'+str(i+1)+'.sh'
         fileName = open(fName, 'a')
         print >>fileName, 'wait'
         fileName.close()
-    for i in range(numOfScript):
-        os.system('qsub sat.pbs -v n='+str(i+1))
+#    for i in range(numOfScript):
+#        os.system('qsub sat.pbs -v n='+str(i+1))
 
 count = 0
 chunkSize = 24
@@ -55,49 +77,74 @@ scriptNo = 0
 fileName = ''
 numOfInstance = 10
 totalJobs = 11400
-rseed = 0
-
+rseed = 1
 
 random.seed(rseed)
-track = writeHeader(totalJobs, chunkSize)
-for k in [0, 2, 4, 8, 16]:
+
+""" the first pass count the number of jobs needed to be run """
+for k in [0, 2, 4, 8]:
     for n in [20, 50, 100] :
         for i in range(10):
             for a in ['LS', 'GA', 'CHC']:
                 for f in ['fit', 'mean', 'std']:
                     for p in ['NKQ','NK']:
                         if p == 'NKQ':
-                            for q in [2, 4, 8, 16]:
+                            for q in [2, 4, 8]:
                                 if a == 'LS':
                                     s = 1
-                                    writeScript(p,a,f,i,s,n,k,q,count,scriptNo,chunkSize, track)
+                                    count =  countJobs(p,a,f,i,s,n,k,q,count)
                                 else: # if the algorithm is population-based, for GA and CHC
                                     for s in [30, 100]:
-                                        writeScript(p,a,f,i,s,n,k,q,count,scriptNo,chunkSize, track)
+                                        count = countJobs(p,a,f,i,s,n,k,q,count)
                         elif p == 'NK': # for NK problem
                             q = 0
                             if a == 'LS':
                                 s = 1
-                                writeScript(p,a,f,i,s,n,k,q,count,scriptNo,chunkSize, track)
+                                count = countJobs(p,a,f,i,s,n,k,q,count)
                             else: # if the algorithm is population-based, for GA and CHC
                                 for s in [30, 100]:
-                                    writeScript(p,a,f,i,s,n,k,q,count,scriptNo,chunkSize, track)
+                                    count = countJobs(p,a,f,i,s,n,k,q,count)
+totalJobs = count
 
-print 'count', count
-# for MAXSAT problem
-instances = random.sample(range(1,1001), numOfInstance)
-p = 'SAT'
-for a in ['LS', 'GA', 'CHC']:
-    for i in instances:
-        if a == 'LS':
-            s =1
-            for f in ['fit', 'mean', 'std']:
-                writeScript(p,a,f,i,s,n,k,q,count,scriptNo,chunkSize, track)
-
-        else: # if the algorithm is population-based, for GA and CHC
-            for s in [30,100]:
+track = writeHeader(totalJobs, chunkSize)
+numOfscript = len(track)
+for k in [0, 2, 4, 8]:
+    for n in [20, 50, 100] :
+        for i in range(10):
+            for a in ['LS', 'GA', 'CHC']:
                 for f in ['fit', 'mean', 'std']:
-                    writeScript(p,a,f,i,s,n,k,q,count,scriptNo,chunkSize, track)
-
+                    for p in ['NKQ','NK']:
+                        if p == 'NKQ':
+                            for q in [2, 4, 8]:
+                                if a == 'LS':
+                                    s = 1
+                                    writeScript(p,a,f,i,s,n,k,q,chunkSize, track)
+                                else: # if the algorithm is population-based, for GA and CHC
+                                    for s in [30, 100]:
+                                        writeScript(p,a,f,i,s,n,k,q,chunkSize, track)
+                        elif p == 'NK': # for NK problem
+                            q = 0
+                            if a == 'LS':
+                                s = 1
+                                writeScript(p,a,f,i,s,n,k,q,chunkSize, track)
+                            else: # if the algorithm is population-based, for GA and CHC
+                                for s in [30, 100]:
+                                    writeScript(p,a,f,i,s,n,k,q,chunkSize, track)
 writeFooter(totalJobs, chunkSize)
-print count, 'jobs submitted', 'separated into', scriptNo, 'scripts'
+
+# for MAXSAT problem
+#instances = random.sample(range(1,1001), numOfInstance)
+#p = 'SAT'
+#for a in ['LS', 'GA', 'CHC']:
+#    for i in instances:
+#        if a == 'LS':
+#            s =1
+#            for f in ['fit', 'mean', 'std']:
+#                count = writeScript(p,a,f,i,s,n,k,q,count,scriptNo,chunkSize, track)
+#
+#        else: # if the algorithm is population-based, for GA and CHC
+#            for s in [30,100]:
+#                for f in ['fit', 'mean', 'std']:
+#                    count = writeScript(p,a,f,i,s,n,k,q,count,scriptNo,chunkSize, track)
+#
+print count, 'jobs submitted', 'separated into', numOfscript, 'scripts'
