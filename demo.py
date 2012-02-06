e116a29cb848b2fe7db9e50c3fe6fa3026f6a620
Wenxiang Chen--Desktop <chenwx.ustc@gmail.com>
2012-02-06 16:57:03 -0700

    initial trial of distance 2 neig

--- demo.py
+++ demo.py
@@ -44,9 +44,9 @@ n = int(tl.getArgv())
 if probName != 'SAT':
     k = int(tl.getArgv())
 
-maxFit = 1000 * n
+maxFit = 1000 * n  * n
 #maxFit = 50
-runs = 30
+runs = 1
 popSize = 50 # always keep popSize to even number
 
 #maxFit = 1000
