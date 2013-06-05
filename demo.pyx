20673ade829269e39e964019f54f6316ed17435f
Wenxiang Chen--Laptop <chenwx.ustc@gmail.com>
2013-06-05 16:14:47 -0600

    [DAT] adjust runs = 10

--- demo.pyx
+++ demo.pyx
@@ -139,15 +139,13 @@ def main():
     # maxFit = opt.e * opt.n
     # runs = 20
     
-    # runs = 10
-    # # maxFit = 1000000                      # 1 million
-    # maxFit = 100000                      # 100 k, submit for running experiment
-    
-    maxFit = 100000
+    runs = 10
+    maxFit = 100000                      # 100 k, submit for running experiment
+    # maxFit = 1000000                      # 1 million
     
     # maxFit = 2000
     
-    runs = 1
+    # runs = 1
     # maxFit = 20000
     
     t = opt.t
